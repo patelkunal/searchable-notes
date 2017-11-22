124a696d49be1443275bd619e11da83568e061c4
kunal <patelkunal89@gmail.com>
2017-11-22 23:56:35 +0530

    add temp work

--- src/main/java/org/coderearth/searchablenotes/Application.java
+++ src/main/java/org/coderearth/searchablenotes/Application.java
@@ -1,10 +1,38 @@
 package org.coderearth.searchablenotes;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import java.io.IOException;
+
 public class Application {
 
-	public static void main(String[] args) {
-		System.out.println("Starting SearchableNotes Application !!");
-		System.out.println("Stopping SearchableNotes Application !!");
-	}
+    public static void main(String[] args) throws IOException {
+        System.out.println("Starting SearchableNotes Application !!");
+
+        final RestHighLevelClient restClient = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http")
+                )
+        );
+
+        TransportClient client = TransportClient
+                .builder()
+                .settings(Settings.builder().build())
+                .build();
+
+        final SearchSourceBuilder sourceBuilder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+        final SearchRequest searchRequest = new SearchRequest("notes").types("note").source(sourceBuilder);
+        final SearchResponse searchResponse = restClient.search(searchRequest);
+        System.out.println(searchResponse.status().getStatus());
+        System.out.println("Stopping SearchableNotes Application !!");
+    }
 
 }
